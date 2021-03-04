ba652dc57429ec2e769d8735c027d6f61b4ddf11
Mikhail Tripolitov <tripolitov@gmail.com>
2021-03-04 16:40:07 +0300

    Dockerize mongodb todo app.
    - added Dockerfile
    - updated index.js to use environment variables for connection params
    - added docker-compose with mongo and mongo-express
    - added docker-compose-atlas with connection to atlas cluster

--- 2.6-mongo/index.js
+++ 2.6-mongo/index.js
@@ -22,14 +22,24 @@ app.use('/api/todo', todoApiRouter);
 app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
-
+const UserDB = process.env.DB_USERNAME || 'root';
+const PasswordDB = process.env.DB_PASSWORD || 'qwerty12345';
+const NameDB = process.env.DB_NAME || 'todos'
+const HostDb = process.env.DB_HOST || 'mongodb://localhost:27017/'
 async function start() {
     try {
-        const PasswordDB = 'xxxxxxxxxx';
-        const NameDB = 'mongoose';
-        const UrlDB = `mongodb+srv://root:${PasswordDB}@cluster0.grfrs.mongodb.net/${NameDB}`;
+        //const UrlDB = `mongodb+srv://${UserDB}:${PasswordDB}@cluster0.grfrs.mongodb.net/${NameDB}`;
         //const UrlDB = `mongodb://localhost:27017/mydb`;
-        await mongoose.connect(UrlDB);
+        //const UrlDB = `mongodb://${UserDB}:${PasswordDB}@localhost:27017/mydb`;
+        //await mongoose.connect(UrlDb);
+        
+        await mongoose.connect(HostDb, {
+            user: UserDB,
+            pass: PasswordDB,
+            dbName: NameDB,
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
 
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
